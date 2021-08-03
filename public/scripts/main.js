7a1e83e43de0a7ff01747942f7cc71a0f08d9d84
W.Carlos <wendcarlos18@hotmail.com>
2021-08-03 11:33:49 -0300

    manipulando modal
    Manipulando a modal com javascript. Os elementos da modal mudam conforme a opção selecionada, Marcar como lida ou excluir.
    Mudanças mo titulo, paragrafo, botão

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,23 +2,35 @@ import Modal from './modal.js'
 
 const modal = Modal()
 
+// Manipulando botão excluir da modal
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const  modalButton = document.querySelector('.modal button')
+
 // Pegar todos os botões que existe com a class check
 const checkButtons = document.querySelectorAll('.actions a.check')
 
 checkButtons.forEach(button => {
     // adicionar a escuta
-    button.addEventListener("click", event => {
-        // Abrir a modal
-        modal.open()
-    })
+    button.addEventListener("click", handleClick)
 })
 
 // Quando o botão delete for clicado ele abre a modal
 const deleteButton = document.querySelectorAll(".actions a.delete")
 
 deleteButton.forEach(button => {
-    button.addEventListener("click", evente => {
-        modal.open()
-    })
+    button.addEventListener("click", (event) => handleClick(event, false))
 })
+
+function handleClick(event, check = true) {
+    const text = check ? "Marcar como lida" : "Excluir"
+
+    modalTitle.innerHTML = `${text} esta pergunta` 
+    modalDescription.innerHTML = `Tem certeza que você deseja ${text.toLocaleLowerCase()} esta pergunta?`
+    modalButton.innerHTML = `Sim, ${text.toLocaleLowerCase()}` 
+    check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
+
+    //abrir modal
+    modal.open()
+}
 
