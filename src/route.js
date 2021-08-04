2b6a05582556f0aa57e9b57352aced9178c06815
W.Carlos <wendcarlos18@hotmail.com>
2021-08-04 11:34:45 -0300

    controller
    Criei a pasta controllers e criei o arquivo QuestionController.js nesse arquivo ficara as regras de negocio da modal

--- src/route.js
+++ src/route.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const QuestionController = require('./controllers/QuestionController')
 
 const route = express.Router()
 
@@ -6,8 +7,7 @@ route.get('/', (req, res) => res.render("index"))
 route.get('/room', (req, res) => res.render("room"))
 route.get('/create-pass', (req, res) => res.render("create-pass"))
 
-/*Formato que o formulario de dentro da modal tem que passar a informação 
-
-route.post('/room/:room/:question/:action')*/
+/*Formato que o formulario de dentro da modal tem que passar a informação */
+route.post('/room/:room/:question/:action', QuestionController.index)
 
 module.exports = route
