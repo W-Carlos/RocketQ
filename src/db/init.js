f1caa6905bb2a7e7824bff2dc28f9ca239b9dd6f
W.Carlos <wendcarlos18@hotmail.com>
2021-08-05 20:02:55 -0300

    tabelas do banco de dados
    Criei as tabelas das salas e das perguntas

---
+++ src/db/init.js
@@ -0,0 +1,24 @@
+const Database = require("./config")
+
+const initDb = {
+    async init(){
+        const db = await Database()
+
+        // Tabela das salas
+        await db.exec(`CREATE TABLE rooms (
+                id INTEGER PRIMARY KEY,
+                pass TEXT
+            )`);
+
+        // Tabela das perguntas
+        await db.exec(`CREATE TABLE questions (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                titulo TEXT,
+                read INT
+            )`);
+        
+        await db.close()
+    }
+}
+
+initDb.init();
